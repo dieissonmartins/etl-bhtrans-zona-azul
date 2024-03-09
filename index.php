c3ed358ce1c1c9bcdf4f0bea4480fc53adeb3928
dieissonmartins <dieisson.martins.santos@gmail.com>
2024-03-09 08:31:19 -0300

    add pacote para request na url

--- index.php
+++ index.php
@@ -3,3 +3,11 @@
 $application = parse_ini_file(__DIR__ . "/.env");
 
 require_once 'vendor/autoload.php';
+
+
+$class = $argv[1];
+$className = "Scripts\\$class";
+
+$instance = new $className();
+
+$instance->runScript();
