3fede5093e5a82035cb4c60f67ede766bb52fa7a
dieissonmartins <dieisson.martins.santos@gmail.com>
2024-03-09 11:43:14 -0300

    formata horas

--- scripts/ImportEstacionamentoRotativo.php
+++ scripts/ImportEstacionamentoRotativo.php
@@ -4,6 +4,8 @@ namespace Scripts;
 
 use Drivers\ConnectionMysql;
 use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use PDO;
 
 class ImportEstacionamentoRotativo extends Script
@@ -23,15 +25,18 @@ class ImportEstacionamentoRotativo extends Script
 
     /**
      * @return bool
+     * @throws GuzzleException
      */
     public function runScript(): bool
     {
         $imports = $this->getPendentImports();
 
         foreach ($imports as $import) {
+            $data = [];
 
-            $debug = $import;
+            $this->extract($import, $data);
 
+            $this->transformData($data);
         }
 
         return true;
@@ -62,5 +67,96 @@ SQL;
         $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param array $import
+     * @param array $data
+     * @return void
+     * @throws GuzzleException
+     */
+    private function extract(array $import, array &$data)
+    {
+        $path = $import['path'];
+
+        echo 'extract items' . PHP_EOL;
+
+        $client = new Client([
+            'headers' => [
+                'User-Agent' => 'insomnia/2023.5.8',
+            ],
+        ]);
+
+        $response = $client->get($path);
+        $csv_file = $response->getBody()->getContents();
+        $temp_file = tempnam(sys_get_temp_dir(), 'csv_');
+
+        file_put_contents($temp_file, $csv_file);
+        $file_handle = fopen($temp_file, 'r');
+
+        $rows = [];
+        $header = false;
+        while (($line = fgets($file_handle)) !== false) {
+            if (!$header) {
+                $header = true;
+                continue;
+            }
+
+            $row = str_getcsv($line);
+            $rows[] = $row;
+        }
+
+        $data = $rows;
+    }
+
+    /**
+     * @param array $data
+     * @return void
+     */
+    private function transformData(array &$data)
+    {
+        echo 'transform data items' . PHP_EOL;
+
+        $aux_data = [];
+        foreach ($data as $key => $row) {
+
+            # format colunn time
+            $time_permanence = '';
+            $time_permanence_aux = $row[3];
+            $time_permanence_aux_arr = explode(' ', $time_permanence_aux);
+            if (isset($time_permanence_aux_arr[1])) {
+                $type = $time_permanence_aux_arr[1];
+                $original_number = $time_permanence_aux_arr[0];
+
+                # horas
+                if ($type === 'HORA(S)') {
+                    $time_permanence = sprintf('%02d:00:00', (int)$original_number);
+                }
+            }
+
+            $aux = [
+                'parking_id' => $row[0],
+                'vacancies_physical_count' => $row[1],
+                'vacancies_rotating_count' => $row[2],
+                'time_permanence_label' => $row[3],
+                'time_permanence' => $time_permanence,
+                'public_place' => $row[4],
+                'reference' => $row[5],
+                'neighborhood' => $row[6],
+                'period_label' => $row[7],
+                'time_period_start',
+                'time_period_end',
+                'day_label' => $row[8],
+                'day_start',
+                'day_end',
+                'polygon' => $row[9],
+            ];
+
+
+            $aux_data[$key] = $aux;
+
+        }
+
+        $data = $aux_data;
     }
 }
--- sqls/parking_lots.sql
+++ sqls/parking_lots.sql
@@ -1,20 +1,21 @@
 create table parking_lots
 (
-    id                       int auto_increment,
-    parking_id               int                                                                           null,
-    vacancies_physical_count int                                                                           null,
-    vacancies_rotating_count int                                                                           null,
-    time_permanence_label    varchar(255)                                                                  null,
-    time_permanence          time                                                                          null,
-    public_place             varchar(500)                                                                  null,
-    reference                varchar(500)                                                                  null,
-    neighborhood             varchar(150)                                                                  null,
-    period_label             varchar(100)                                                                  null,
-    time_period_start        time                                                                          null,
-    time_period_end          time                                                                          null,
-    day_label                varchar(100)                                                                  null,
-    day                      enum ('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday') null,
-    polygon                  varchar(250)                                                                  null,
-    constraint parking_lots_pk
-        primary key (id)
+    id                       int auto_increment
+        primary key,
+    csv_import_id            int                                                                                 null,
+    parking_id               int                                                                                 null,
+    vacancies_physical_count int                                                                                 null,
+    vacancies_rotating_count int                                                                                 null,
+    time_permanence_label    varchar(255)                                                                        null,
+    time_permanence          time                                                                                null,
+    public_place             varchar(500)                                                                        null,
+    reference                varchar(500)                                                                        null,
+    neighborhood             varchar(150)                                                                        null,
+    period_label             varchar(100)                                                                        null,
+    time_period_start        time                                                                                null,
+    time_period_end          time                                                                                null,
+    day_label                varchar(100)                                                                        null,
+    day_start                enum ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday') null,
+    day_end                  enum ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday') null,
+    polygon                  varchar(250)                                                                        null
 );
